b49689f41a04122051f71eadbacac87d028a8be3
Oriol Capdevila <36154523+OriolCS2@users.noreply.github.com>
2026-04-26 18:46:42 +0200

    docs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './', // Use relative paths for assets
   build: {
-    outDir: 'build',
+    outDir: 'docs',
     emptyOutDir: true,
   }
 });
